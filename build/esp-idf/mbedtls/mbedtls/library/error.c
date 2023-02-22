e4e7c24a3ee623ff05b9fbb42064aa06cdb8bd4e
JeffeeH <j22132235@gmail.com>
2023-02-22 18:53:44 +0100

    deleted build folder

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/Users/jeffeehsiung/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/jeffeehsiung/esp/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
