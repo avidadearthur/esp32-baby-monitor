7da90f19ad50a24476a83d4e039cbd973091e40e
JeffeeH <j22132235@gmail.com>
2023-03-24 18:56:48 +0100

    the audio quality was fixed by setting the i2s clock source to ((adc_bits+8)/16) times the sample rate, which is 1.25 times the sample rate. reference: https://docs.espressif.com/projects/esp-idf/en/v4.2/esp32/api-reference/peripherals/i2s.html#_CPPv49i2s_write10i2s_port_tPKv6size_tP6size_t10TickType_t

--- main/espnow_mic.h
+++ main/espnow_mic.h
@@ -6,7 +6,7 @@
 void i2s_adc_data_scale(uint8_t * des_buff, uint8_t* src_buff, uint32_t len);
 void i2s_adc_capture_task(void* task_param);
 void i2s_dac_playback_task(void* task_param);
-esp_err_t init_audio_trans(StreamBufferHandle_t mic_stream_buf);
+esp_err_t init_audio_trans(StreamBufferHandle_t mic_stream_buf, StreamBufferHandle_t fft_audio_buf, StreamBufferHandle_t record_stream_buf);
 esp_err_t init_audio_recv(StreamBufferHandle_t network_stream_buf);
 void mic_disp_buf(uint8_t* buf, int length);
 
