df2a47a48a1f6c6568ed021222598ef445b6fe39
JeffeeH <j22132235@gmail.com>
2023-03-26 12:44:57 +0200

    reupdate espnow_mic, main, fftPeak. somehow the files did not merge properly

--- main/fftpeak.h
+++ main/fftpeak.h
@@ -6,6 +6,14 @@
 #include <complex.h>
 #include "config.h"
 #include "fft.h"
+#include "driver/spi_master.h"
+#include "soc/gpio_struct.h"
+#include "driver/uart.h"
+#include "soc/uart_struct.h"
+#include <math.h>
+
+#include "esp_dsp.h"
+
 
 void init_fft(StreamBufferHandle_t fft_stream_buf);
 
