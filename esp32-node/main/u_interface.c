2c9602bf6d98463dd96368e8ccfdf190a1474a78
Arthur Tavares <arthurtquintao@gmail.com>
2023-03-30 17:28:16 +0200

    Add functionality to toggle music state ON and OFF

--- esp32-node/main/u_interface.c
+++ esp32-node/main/u_interface.c
@@ -19,6 +19,7 @@ typedef enum
     SET_MIN_TEMP_THRESHOLD,
     DISPLAY_MUSIC_STATE,
     SET_MUSIC_STATE,
+    SET_MUSIC_CONFIRM_STATE
 } fsm_state_t;
 
 fsm_state_t current_state = DISPLAY_HOME_STATE;
@@ -29,6 +30,17 @@ bool interrupt_flag = false;
 uint32_t last_button_isr_time = 0;
 
 static const char *TAG = "u_interface.c";
+
+/* User defined global constants */
+// define temp threshold as global variable
+float temp_threshold_min = 22.0;
+float temp_threshold_max = 24.0;
+// define current max and min temp as global variable
+float current_max_temp = 0.0;
+float current_min_temp = 0.0;
+
+// define music state as global variable
+bool music_state = false;
 
 void IRAM_ATTR up_button_isr_handler(void *arg)
 {
@@ -53,7 +65,6 @@ void IRAM_ATTR up_button_isr_handler(void *arg)
         {
             current_state = DISPLAY_MUSIC_STATE;
         }
-
         xSemaphoreGiveFromISR(xSemaphore, &xHigherPriorityTaskWoken);
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 
@@ -92,6 +103,57 @@ void IRAM_ATTR down_button_isr_handler(void *arg)
     }
 }
 
+void IRAM_ATTR set_button_isr_handler(void *arg)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    uint32_t isr_time = xTaskGetTickCountFromISR();
+
+    if ((isr_time - last_button_isr_time) > pdMS_TO_TICKS(BUTTON_DEBOUNCE_TIME_MS))
+    {
+        if (current_state == DISPLAY_MUSIC_STATE)
+        {
+            current_state = SET_MUSIC_STATE;
+        }
+        else if (current_state == SET_MUSIC_STATE)
+        {
+            // toggle the music state
+            music_state = !music_state;
+            current_state = SET_MUSIC_CONFIRM_STATE;
+        }
+
+        xSemaphoreGiveFromISR(xSemaphore, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+
+        last_button_isr_time = isr_time; // Save last ISR time
+    }
+}
+
+void IRAM_ATTR cancel_button_isr_handler(void *arg)
+{
+
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    uint32_t isr_time = xTaskGetTickCountFromISR();
+
+    if ((isr_time - last_button_isr_time) > pdMS_TO_TICKS(BUTTON_DEBOUNCE_TIME_MS))
+    {
+        if (current_state == SET_MUSIC_STATE)
+        {
+            current_state = DISPLAY_MUSIC_STATE;
+        }
+        else if (current_state == SET_MUSIC_CONFIRM_STATE)
+        {
+            // toggle the music state
+            music_state = !music_state;
+            current_state = DISPLAY_MUSIC_STATE;
+        }
+
+        xSemaphoreGiveFromISR(xSemaphore, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+
+        last_button_isr_time = isr_time; // Save last ISR time
+    }
+}
+
 void init_u_interface(void)
 {
     // Initialize semaphore
@@ -101,12 +163,14 @@ void init_u_interface(void)
     io_conf.intr_type = GPIO_INTR_POSEDGE;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
-    io_conf.pin_bit_mask = (1ULL << UP_BUTTON_GPIO) | (1ULL << DOWN_BUTTON_GPIO);
+    io_conf.pin_bit_mask = (1ULL << UP_BUTTON_GPIO) | (1ULL << DOWN_BUTTON_GPIO) | (1ULL << SET_BUTTON_GPIO) | (1ULL << CANCEL_BUTTON_GPIO);
     gpio_config(&io_conf);
 
     gpio_install_isr_service(0);
     gpio_isr_handler_add(UP_BUTTON_GPIO, up_button_isr_handler, NULL);
     gpio_isr_handler_add(DOWN_BUTTON_GPIO, down_button_isr_handler, NULL);
+    gpio_isr_handler_add(SET_BUTTON_GPIO, set_button_isr_handler, NULL);
+    gpio_isr_handler_add(CANCEL_BUTTON_GPIO, cancel_button_isr_handler, NULL);
 
     while (1)
     {
@@ -127,37 +191,80 @@ void init_u_interface(void)
                 // Code for DISPLAY_EXTREME_TEMP
                 // log the current state
                 ESP_LOGI(TAG, "Current state: DISPLAY_EXTREME_TEMP");
+
+                // log the value of the current maximum temperature
+                ESP_LOGI(TAG, "current_max_temp: %.2f", current_max_temp);
+
+                // log the value of the current minimum temperature
+                ESP_LOGI(TAG, "current_min_temp: %.2f", current_min_temp);
                 break;
             case DISPLAY_MAX_TEMP_THRESHOLD:
                 // Code for DISPLAY_MAX_TEMP_THRESHOLD
                 // log the current state
                 ESP_LOGI(TAG, "Current state: DISPLAY_MAX_TEMP_THRESHOLD");
+
+                // log the values of the temp thresholds
+                ESP_LOGI(TAG, "temp_threshold_max: %.2f", temp_threshold_max);
                 break;
+
+            /*editing state*/
             case SET_MAX_TEMP_THRESHOLD:
                 // Code for SET_MAX_TEMP_THRESHOLD
                 // log the current state
                 ESP_LOGI(TAG, "Current state: SET_MAX_TEMP_THRESHOLD");
+
                 break;
+
             case DISPLAY_MIN_TEMP_THRESHOLD:
                 // Code for DISPLAY_MIN_TEMP_THRESHOLD
                 // log the current state
                 ESP_LOGI(TAG, "Current state: DISPLAY_MIN_TEMP_THRESHOLD");
+
+                // log the values of the temp thresholds
+                ESP_LOGI(TAG, "temp_threshold_min: %.2f", temp_threshold_min);
                 break;
+
+            /*editing state*/
             case SET_MIN_TEMP_THRESHOLD:
                 // Code for SET_MIN_TEMP_THRESHOLD
                 // log the current state
                 ESP_LOGI(TAG, "Current state: SET_MIN_TEMP_THRESHOLD");
+
                 break;
+
             case DISPLAY_MUSIC_STATE:
                 // Code for DISPLAY_MUSIC_STATE
                 // log the current state
                 ESP_LOGI(TAG, "Current state: DISPLAY_MUSIC_STATE");
+
+                // log the music state
+                ESP_LOGI(TAG, "Music state: %s", music_state ? "ON" : "OFF");
                 break;
+
+            /*editing state*/
             case SET_MUSIC_STATE:
                 // Code for SET_MUSIC_STATE
                 // log the current state
                 ESP_LOGI(TAG, "Current state: SET_MUSIC_STATE");
+
+                // log the current music state
+                ESP_LOGI(TAG, "Music state: %s", music_state ? "ON" : "OFF");
+
+                // log "Change music state? Press SET to confirm."
+                ESP_LOGI(TAG, "Change music state? Press SET to confirm.");
                 break;
+            case SET_MUSIC_CONFIRM_STATE:
+                // Code for SET_MUSIC_CONFIRM_STATE
+                // log the current state
+                ESP_LOGI(TAG, "Current state: SET_MUSIC_CONFIRM_STATE");
+
+                // log the current music state
+                ESP_LOGI(TAG, "Music state changed: %s", music_state ? "ON" : "OFF");
+
+                // go back to the DISPLAY_MUSIC_STATE
+                current_state = DISPLAY_MUSIC_STATE;
+                break;
+
             default:
                 break;
             }
